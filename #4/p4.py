dd0d784496862cf8b4968c64d85ad117b0732320
Mamba-Bajamba <croserpaul@gmail.com>
2021-02-05 12:46:59 +1100

    Problem 4 solution

---
+++ #4/p4.py
@@ -0,0 +1,48 @@
+"""
+Project Euler: Problem 4
+Problem link (specification): https://projecteuler.net/problem=4
+Author: Hamish Croser
+"""
+
+"""
+Finds the largest palindrome made from the product of two numbers within the scope
+scope: list of int - only numbers between the given values (inclusive) will be considered as factors
+largest: int - largest palindrome made from the product of two numbers within the scope
+"""
+def largest_palindrome(scope):
+	palindromes = []
+	for i in range(scope[0],scope[1]+1):
+		for j in range(scope[0],scope[1]+1):
+			if is_palindrome(i*j):
+				palindromes.append(i*j)
+	palindromes.sort()
+	largest = palindromes[-1]
+	return largest
+
+"""
+Checks if the given candidate is a palindrome
+candidate: int - the number that will be checked
+is_palindrome: boolean - will return True if the candidate is a palindrome
+"""
+def is_palindrome(candidate):
+	digits = [int(i) for i in str(candidate)]
+	is_palindrome = True
+	for i in range(len(digits)):
+		if digits[i] != digits[-(i+1)]:
+			is_palindrome = False
+			break
+	return is_palindrome
+
+"""
+Called directly by the program. This function calls largest_palindrome() with the required argument
+as provided in the specification and prints the result.
+"""
+def main():
+	scope = [100,999]
+	largest = largest_palindrome(scope)
+	print("Largest palindrome: {}".format(largest))
+
+import time
+start = time.time()
+main()
+print("Runtime: %.3f seconds" % (time.time() - start))
